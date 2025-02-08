ee4fc4b60de45db3630fa5aee99be87fe2412137
Justin Fiset <justinfiset@hotmail.fr>
2025-02-07 23:15:08 -0500

    meilleures strucutre des routes

--- routes.php
+++ routes.php
@@ -2,31 +2,31 @@
 
 // Spécification de chacunes des routes pour les différents fichiers php en fonction de la méthode HTTP (GET, POST, PUT, DELETE)
 // chaque clé est un regex et chaque valeur est le chemin du fichier php correspondant qui sera chargé
-function get_routes($method) {
-    if($method == "GET") {
-        return [
-            "/index/?" => "/public/index.php",
-            "/formulaireActivite/?" => "/public/formulaireActivite.php",
-            "/listeActivite/?" => "/public/listeActivite.php",
+$routes = [
+    "GET" => [
+        "/index/?" => "/public/index.php",
+        "/formulaireActivite/?" => "/public/formulaireActivite.php",
+        "/listeActivite/?" => "/public/listeActivite.php",
+        "/api/activities/random/?" => "/api/activities/randomActivities.php",
+        "/api/activities/filter/?" => "/api/activities/filterActivities.php",
+        "/api/activities/([0-9]+/?)" => "/api/activities/activity.php",
+        "/api/coaches/?" => "/api/coaches/coaches.php",
+        "/api/locations/?" => "/api/locations/locations.php",
+        "/404" => "/404.php",
+    ],
+    "POST" => [
+        "/api/activities/?" => "/api/activities/addActivity.php",
+        "/404" => "/404.php",
+    ],
+    "PUT" => [
+        "/api/activities/([0-9]+)" => "/api/activities/updateActivity.php",
+        "/404" => "/404.php",
+    ],
+];
 
-            "/api/activities/random/?" => "/api/activities/randomActivities.php",
-            "/api/activities/filter/?" => "/api/activities/filterActivities.php",
-            "/api/activities/([0-9]+/?)" => "/api/activities/activity.php",
-            "/api/coaches/?" => "/api/coaches/coaches.php",
-            "/api/locations/?" => "/api/locations/locations.php",
-            "/404" => "/404.php",
-        ];
-    } else if($method == "POST") { 
-        return [
-            "/api/activities/?" => "/api/activities/addActivity.php",
-            "/404" => "/404.php",
-        ];
-    } else if($method == "PUT") {
-        return [
-            "/api/activities/([0-9]+)" => "/api/activities/updateActivity.php",
-            "/404" => "/404.php",
-        ];
-    }
+function get_routes($method) {
+    global $routes;
+    return $routes[$method] ?? [];
 }
 
 ?>
