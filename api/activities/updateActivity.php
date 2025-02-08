92ceab534a2610ef24bb8a40373bf5db94252f5e
AimeMelancon <aimemelancon@gmail.com>
2025-02-07 21:39:21 -0500

    début updateActivity
    J'ai commencé à faire  pour update les activités, mais faudrait que je comprenne comment utiliser les json avant de continuer, je crois ?

--- api/activities/updateActivity.php
+++ api/activities/updateActivity.php
@@ -1,13 +1,27 @@
-<?php 
+<?php
 require __DIR__ . "/../database.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if($data){
+if ($data) {
 
-$req = $pdo ->prepare(
-    "UPDATE activities SET   ");
+    $req = $pdo->prepare(
+        "UPDATE activities 
+            SET   name = :name, description = :description, level_id = :level_id, coach_id = :coach_id, location_id = :location_id
+            WHERE activities.id = :activities_id"
+    );
+
+    $req->execute([
+        "name" => $data["name"],
+        "description" => $data["description"],
+        "level_id" => $data["level_id"],
+        "coach_id" => $data["coach_id"],
+        "location_id" => $data["location_id"],
+        "activities_id"=> $data["activities_id"],
+    ]);
+
+    $rep = $req->fetchAll(PDO::FETCH_ASSOC); 
+
+    
 
 }
-
-?>
