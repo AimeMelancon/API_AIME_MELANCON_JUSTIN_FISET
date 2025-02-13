6d293ccd09379fe2dbf6544897db274924faa0d2
AimeMelancon <aimemelancon@gmail.com>
2025-02-12 20:25:38 -0500

    robustesse pour la création d'activité et la modification de celle-ci

--- utils.php
+++ utils.php
@@ -1,9 +1,15 @@
 <?php
 
-function isValid(...$values) {
-    foreach($values as $value) {
-        if(isset($value)) {
-            if(is_string($value) && empty($value)) {
+function isValid(...$values)
+{
+    foreach ($values as $value) {
+        if (isset($value)) {
+            if (is_string($value) && empty(trim($value))) {
+
+                http_response_code(400);
+                echo json_encode([
+                    "status" => "JSON invalide, vous devez fournir une activité valide au format JSON."
+                ]);
                 return false;
             }
         }
